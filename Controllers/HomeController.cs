b62d1aa5a03201c151ed54735f7377e357e7e071
Thilomax <thove6344@oslomet.no>
2026-09-03 14:21:57 +0200

    Lecture 2 frem til Create Shared Layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,9 +5,11 @@ namespace MyShop.Controllers
     public class HomeController : Controller
     {
         // GET: /<controller>/
+        //IActionResult is the action method called index. (I for interface. this method sends the view on the home page.)
         public IActionResult Index()
         {
             return View();
+            //Since this method is in HomeController, and the name is Index, it will return the view in /Home/Index
         }
     }
 }
