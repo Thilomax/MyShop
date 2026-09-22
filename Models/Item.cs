21fa11fe40cb1e9039975968532f7962827e8b82
Thilomax <thove6344@oslomet.no>
2026-09-22 10:03:53 +0200

    fixed navigation property not being virtual

--- Models/Item.cs
+++ Models/Item.cs
@@ -18,6 +18,6 @@ namespace MyShop.Models
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
         //Navigation property
-        public List<OrderItem>? OrderItems { get; set; }
+        public virtual List<OrderItem>? OrderItems { get; set; }
     }
 }
