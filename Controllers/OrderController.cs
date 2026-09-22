d22f5f7f7d88f925e8006eab544514881a006b55
Thilomax <thove6344@oslomet.no>
2026-09-22 12:37:48 +0200

    Demo 5 - Expanded CRUD finished

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using MyShop.Models;
 using Microsoft.EntityFrameworkCore;
+using MyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MyShop.Controllers;
 
@@ -20,9 +22,27 @@ public class OrderController : Controller
     }
 
     [HttpGet]
-    public IActionResult CreateOrderItem() //Displays the create order view
+    public async Task<IActionResult> CreateOrderItem() //Displays the create order view in the async way
     {
-        return View();
+        var items = await _itemDbContext.Items.ToListAsync(); //retrieves items and orders and converts to list
+        var orders = await _itemDbContext.Orders.ToListAsync(); 
+        var createOrderItemViewModel = new CreateOrderItemViewModel //creates new CreateOrderItemViewModel, three variables: OrderItem, ItemSelectList, OrderSelectList
+        {
+            OrderItem = new OrderItem(),
+
+            ItemSelectList = items.Select(item => new SelectListItem //this wraps the information stored in "items" into the ItemSelectList
+            {
+                Value = item.ItemId.ToString(), // each SlectListItem has a value that is the content of the item and a TEXT that displays the time
+                Text = item.ItemId.ToString() + ": " + item.Name
+            }).ToList(),
+            
+            OrderSelectList = orders.Select(order => new SelectListItem //wraps the information of OrderIds, OrderDates and customer names into OrderSelectList
+            {
+                Value = order.OrderId.ToString(),
+                Text = "Order" + order.OrderId.ToString() + ", Date: " + order.OrderDate + ", Customer: "+ order.Customer.Name
+            }).ToList(),
+        };
+        return View(createOrderItemViewModel);
     }
 
     [HttpPost] //this following method is important. if users input ItemIds or OrderIds that dont exist in the DB, the OrderItem cant be created.
